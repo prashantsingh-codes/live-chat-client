5e24b505da4204f010eb7dc2f38f080087d0f756
Prashant Singh <prashant.singh.codes@gmail.com>
2026-05-03 13:30:36 +0530

    logout UI changed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -223,7 +223,7 @@ const Sidebar = () => {
             <polyline points="16 17 21 12 16 7" />
             <line x1="21" y1="12" x2="9" y2="12" />
           </svg>
-          <span className="logout-label">Logout {userData?.name ? `${userData.name}` : ""}</span>
+          <span className="logout-label">Logout, {userData?.name ? `${userData.name}` : ""}</span>
         </button>
       </div>
     </div>
